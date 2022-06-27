7b7d9b1cd703ddbe480e14f17476e1bde1210ee0
Lev Kuzmin <levnc5@gmail.com>
2022-06-27 19:54:25 +0530

    Split data preparation and formatting into separate modules

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import parse from './parsers.js';
 import {
   getPath, getExt,
 } from './utils.js';
-import calcDiff from './calcDiff.js';
+import prepareData from './prepare-data.js';
+import formatter from './formatters/index.js';
 
 export default (file1, file2, format = 'stylish') => {
   const [data1, data2] = [rfs(getPath(file1), 'utf8'), rfs(getPath(file2), 'utf8')];
   const [ext1, ext2] = [getExt(file1), getExt(file2)];
   const [parsed1, parsed2] = [parse(data1, ext1), parse(data2, ext2)];
 
-  if (format === 'stylish') {
-    return calcDiff(parsed1, parsed2);
-  }
-  return `Format "${format}" is not defined.`;
+  const data = prepareData(parsed1, parsed2);
+  return formatter(data, format);
 };
