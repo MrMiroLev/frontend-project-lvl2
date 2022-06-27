8a7dfdd04ab1bc88d695196b906b2ea7a2a94bb5
Lev Kuzmin <levnc5@gmail.com>
2022-06-27 19:51:25 +0530

    Update utils

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,16 @@ import _ from 'lodash';
 const getPath = (file) => path.resolve(file);
 const getExt = (file) => path.extname(file);
 
-const getValue = (obj, key) => _.get(obj, key);
-const isExist = (value) => value !== undefined;
+const hasKey = (obj, key) => _.has(obj, key);
 const isPrimitive = (value) => !_.isObject(value);
 const isEqual = (value1, value2) => value1 === value2;
+const isString = (data) => typeof data === 'string';
+const normalizeValue = (value) => {
+  if (isString(value)) return `'${value}'`;
+  if (isPrimitive(value)) return `${value}`;
+  return '[complex value]';
+};
 
 export {
-  getPath, getExt, getValue, isExist, isPrimitive, isEqual,
+  getPath, getExt, hasKey, isPrimitive, isEqual, isString, normalizeValue,
 };
