07424ff4b14f7c1f29ba593bdd49cfccf0f5081d
Lev Kuzmin <levnc5@gmail.com>
2022-06-27 19:50:37 +0530

    Add constant for format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,7 +12,8 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2, program.opts().format));
+    const formatName = program.opts().format;
+    console.log(genDiff(filepath1, filepath2, formatName));
   });
 
 program.parse();
