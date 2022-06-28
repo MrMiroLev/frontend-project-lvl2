dc8c8149ee9519cbec6c70ee25968d3b5d6bafcd
Lev Kuzmin <levnc5@gmail.com>
2022-06-28 13:33:00 +0530

    Code improvements
    Remove "magic" numbers and make code more readable

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,26 +2,33 @@ import { isPrimitive } from '../utils.js';
 
 export default (data, spacesCount = 4, replacer = ' ') => {
   const iter = (currentData, depth) => {
-    if (isPrimitive(currentData)) return currentData;
+    if (isPrimitive(currentData)) {
+      return currentData;
+    }
 
     const indentSize = spacesCount * depth;
     const emptyIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const minusIndent = `${emptyIndent.slice(0, -2)}- `;
-    const plusIndent = `${emptyIndent.slice(0, -2)}+ `;
+
+    const [firstIndex, secondLastIndex] = [0, -2];
+    const minusIndent = `${emptyIndent.slice(firstIndex, secondLastIndex)}- `;
+    const plusIndent = `${emptyIndent.slice(firstIndex, secondLastIndex)}+ `;
 
     const result = currentData.flatMap(({
       key, status, value, oldValue, newValue,
     }) => {
       switch (status) {
-        case 'added': return `${plusIndent}${key}: ${iter(value, depth + 1)}`;
-        case 'removed': return `${minusIndent}${key}: ${iter(value, depth + 1)}`;
+        case 'added':
+          return `${plusIndent}${key}: ${iter(value, depth + 1)}`;
+        case 'removed':
+          return `${minusIndent}${key}: ${iter(value, depth + 1)}`;
         case 'changed':
           return [
             `${minusIndent}${key}: ${iter(oldValue, depth + 1)}`,
             `${plusIndent}${key}: ${iter(newValue, depth + 1)}`,
           ];
-        default: return `${emptyIndent}${key}: ${iter(value, depth + 1)}`;
+        default:
+          return `${emptyIndent}${key}: ${iter(value, depth + 1)}`;
       }
     });
 
