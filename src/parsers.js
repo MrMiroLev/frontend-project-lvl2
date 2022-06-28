d51f892ed57d933185941d055352eee3e609d86e
Lev Kuzmin <levnc5@gmail.com>
2022-06-28 13:57:03 +0530

    Improve code readability

--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,7 @@ import yaml from 'js-yaml';
 
 export default (data, ext) => {
   switch (ext) {
-    case 'json':
-      return JSON.parse(data);
-    default:
-      return yaml.load(data);
+    case 'json': return JSON.parse(data);
+    default: return yaml.load(data);
   }
 };
