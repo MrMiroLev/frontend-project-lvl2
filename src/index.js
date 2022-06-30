9a83790d909f797937dd3aa03dd81b82a7bdd4de
Lev Kuzmin <levnc5@gmail.com>
2022-06-30 12:32:23 +0530

    Improve code readability
    Change name of function from "formatter" to "formatData" (make it verbal)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import {
 } from './utils.js';
 import parse from './parsers.js';
 import prepareData from './prepare-data.js';
-import formatter from './formatters/index.js';
+import formatData from './formatters/index.js';
 
 export default (file1, file2, format = 'stylish') => {
   const firstFileData = parse(readFile(file1), getExt(file1));
   const secondFileData = parse(readFile(file2), getExt(file2));
 
   const data = prepareData(firstFileData, secondFileData);
-  return formatter(data, format);
+  return formatData(data, format);
 };
